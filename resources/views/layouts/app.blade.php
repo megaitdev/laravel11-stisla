de9637a1421ced6240d9751295c0cbaa9e594867
Abima Nugraha <abimanugraha@gmail.com>
2024-05-31 10:11:44 +0700

    build: init laravel11 + stisla + breeze

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{ asset('library/bootstrap/dist/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/components.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/skins/reverse.css') }}">
+    <link rel="stylesheet" href="{{ asset('library/fontawesome/css/all.min.css') }}">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>@yield('title') &mdash; Laravel - Stisla</title>
+
+    <!-- Fonts -->
+    <link rel="stylesheet" href="{{ asset('css/font.css') }}">
+
+    <!-- Scripts -->
+    <!-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) -->
+</head>
+
+<body class="sidebar-mini">
+    <div id="app">
+        <div class="main-wrapper">
+            <!-- Header -->
+            @include('components.header')
+
+            <!-- Sidebar -->
+            @include('components.sidebar')
+
+            <!-- Content -->
+            @yield('content')
+
+            <!-- Footer -->
+            @include('components.footer')
+        </div>
+    </div>
+
+    <!-- General JS Scripts -->
+    <script src="{{ asset('library/jquery/dist/jquery.min.js') }}"></script>
+    <script src="{{ asset('library/popper.js/dist/umd/popper.js') }}"></script>
+    <script src="{{ asset('library/tooltip.js/dist/umd/tooltip.js') }}"></script>
+    <script src="{{ asset('library/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('library/jquery.nicescroll/dist/jquery.nicescroll.min.js') }}"></script>
+    <script src="{{ asset('library/moment/min/moment.min.js') }}"></script>
+    <script src="{{ asset('js/stisla.js') }}"></script>
+
+    @stack('scripts')
+
+    <!-- Template JS File -->
+    <script src="{{ asset('js/scripts.js') }}"></script>
+    <script src="{{ asset('js/custom.js') }}"></script>
+</body>
+
+</html>
